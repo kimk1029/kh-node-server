a0659de7e05988f618abbffd6c5e71861f0ea159
kimk1029 <kimk1029@naver.com>
2024-12-27 11:04:44 +0900

    fix: 미들웨어 수정

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,8 +7,8 @@ import authMiddleware from "../middlewares/authMiddleware";
 const router = Router();
 
 // 모든 사용자 조회 (인증 필요)
-router.get("/", authMiddleware, getAllUsers);
-
+//router.get("/", authMiddleware, getAllUsers);
+router.get("/", getAllUsers);
 // 특정 사용자 조회 (인증 필요, 선택 사항)
 router.get("/:id", authMiddleware, getUserById);
 
