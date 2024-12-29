be7fa6ef6909ae51ceb89b73cfa5f2631fab9abd
kimk1029 <kimk1029@naver.com>
2024-12-30 08:22:34 +0900

    fix:  config

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -14,13 +14,29 @@ const connectionOptions: ConnectionOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [Post, User], // 엔티티 추가
-  migrations: ["src/migrations/**/*.ts"], // 마이그레이션 파일 경로
+  entities: [__dirname + "/entities/*.js"],
+  migrations: [__dirname + "/migrations/*.js"],
   cli: {
     migrationsDir: "src/migrations",
   },
-  synchronize: false, // 마이그레이션 사용을 위해 false로 설정
+  synchronize: true, // 마이그레이션 사용을 위해 false로 설정
   logging: false,
 };
 
 module.exports = connectionOptions; // CommonJS 방식으로 내보내기
+
+// createConnection({
+//     type: "mysql",
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT) || 3306,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     entities: [__dirname + "/entities/*.js"],
+//     migrations: [__dirname + "/migrations/*.js"],
+//     cli: {
+//       migrationsDir: "src/migration",
+//     },
+//     synchronize: false, // 마이그레이션 사용을 위해 false로 설정// 개발 환경에서만 사용 (프로덕션에서는 마이그레이션 사용 권장)
+//     logging: false,
+//   })
