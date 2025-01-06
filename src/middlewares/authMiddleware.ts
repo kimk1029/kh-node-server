3a2544cb45cd85d43a5faf6e55e835f6d65e238e
kimk1029 <kimk1029@naver.com>
2025-01-06 13:18:00 +0900

    미들웨어 로그 삭제

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -13,29 +13,21 @@ const authMiddleware = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  console.log("AUTH MIDDLEWARE", req.headers);
   const authHeader = req.headers.authorization;
 
-  console.log("AUTH1");
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     res.status(401).json({ message: "Unauthorized:: No token" });
     return;
   }
-  console.log("AUTH2");
   const token = authHeader.split(" ")[1];
-  console.log("AUTH3");
   try {
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
-    console.log("AUTH8");
     const userRepository = getRepository(User);
-    console.log("AUTH9");
     const user = await userRepository.findOne(decoded.id);
-    console.log("AUTH4");
     if (!user) {
       res.status(401).json({ message: "Unauthorized:: User not found" });
       return;
     }
-    console.log("AUTH5");
 
     req.user = user;
     next();
