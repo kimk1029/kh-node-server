0bfb45722fb93dd0f5e33c3d554be533d18c3be2
kimk1029 <kimk1029@naver.com>
2025-01-06 13:01:34 +0900

    콘솔추가

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -16,22 +16,24 @@ const authMiddleware = async (
   console.log("AUTH MIDDLEWARE");
   const authHeader = req.headers.authorization;
 
+  console.log("AUTH123");
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     res.status(401).json({ message: "Unauthorized:: No token" });
     return;
   }
-
+  console.log("AUTH123");
   const token = authHeader.split(" ")[1];
 
   try {
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
     const userRepository = getRepository(User);
     const user = await userRepository.findOne(decoded.id);
-
+    console.log("AUTH12");
     if (!user) {
       res.status(401).json({ message: "Unauthorized:: User not found" });
       return;
     }
+    console.log("AUTH1");
 
     req.user = user;
     next();
