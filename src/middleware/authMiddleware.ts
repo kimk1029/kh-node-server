ecd2335ab36c4b92ec70dc24c1d1788ef60216d2
kimk1029 <kimk1029@naver.com>
2025-05-06 16:50:51 +0900

    fix: authMiddleware에서 로그 메시지 수정 및 추가

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -24,9 +24,10 @@ const authMiddleware = async (
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
       id: string;
     };
-    console.log("[r][e][q]");
+    console.log("[r][e][q]1");
     req.user = { id: decoded.id };
     next();
+    console.log("[r][e][q]44")
   } catch (error) {
     return res.status(401).json({ message: "유효하지 않은 토큰입니다." });
   }
