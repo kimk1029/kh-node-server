f472dcdac49f083d3e5c9e9e4a1228059a46ac35
kimk1029 <kimk1029@naver.com>
2025-05-06 16:53:23 +0900

    fix: 업로드 디렉토리 존재 여부 확인 및 생성 추가

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 import {
   createPost,
   deletePost,
@@ -19,7 +20,13 @@ const router = Router();
 
 // Multer 설정
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
+  destination: (req, file, cb) => {
+    const uploadDir = "uploads";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
   filename: (req, file, cb) => {
     const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, `image-${unique}${path.extname(file.originalname)}`);
